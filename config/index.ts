2ad2be2179a92e305d92185befac8bc74aa16f0f
Techathon <106890862+techathonian@users.noreply.github.com>
2022-11-03 11:37:22 -0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ export const APP_NAME = "Techathon";
 export const DESCRIPTION =
   "Techathon, techathon, Techathon community, Techathononepointzero,mentorship, opensource, opensource community, mentorship and opensource, how to transition into tech, learn new skill, internship, internship programs, intership programs 2022, learn ui/ux, learn product design, learn html, css and JavaScript, learn node js, learn react js, learn data analysis, learn technical writting, learn graphics design, learn Microsoft word, learn python, learn CRM, learn content creation, learn digital marketing ";
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_BASE_URL || "https://verifycert.techathonian.com";
 export const MAIN_SITE = "https://techathonian.com/";
 export const EMAIL = "hello@techathonian.com";
 export const MAX_UPLOAD_SIZE = {
